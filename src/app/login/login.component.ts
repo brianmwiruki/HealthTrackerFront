a8a69307378b06560793d2dca37e5cabaddfc648
Brian Mwiruki <mwiruki@outlook.com>
2020-04-09 12:00:24 +0300

    login initialization - login form implementation in the login components in the html file and initializing binding to functions in the ts file for fetching the user details

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  username = ''
+  password = ''
+  loginUserData = {}
+  constructor(private _auth: AuthService,
+              private _router: Router) { }
 
   ngOnInit() {
   }
+  loginUser(){
+    this._auth.loginUser(this.loginUserData)
+    .subscribe(
+      res => {
+        console.log(res)
+        window.localStorage.setItem('token', res.token)
+        this._router.navigate(['/neighbourhood/post'])
+      },
+      err => console.log(err)
 
+
+    )
+
+  }
 }
